8fbb40892a746c2b38ade5e2fb96caadd1371a00
Mike Oram <Mikeoram01@gmail.com>
2023-01-03 12:11:25 +0000

    Started creating the selection screen

--- src/Components/StartScreen/StartScreen.tsx
+++ src/Components/StartScreen/StartScreen.tsx
@@ -1,12 +1,14 @@
 import Container from "../SharedComponents/Container/Container";
 import './StartScreen.css';
 import Logo from "../SharedComponents/Logo/Logo";
-import LogoSrc from '../../../public/logo.svg'
 
 const StartScreen = (): JSX.Element => {
     return (
         <Container className={"start-game-container"}>
             <Logo className={"logo"} src={'../../../public/logo.svg'}/>
+            <Container className={"start-game-selection-container"}>
+                <p className={"selection-text"}>PICK PLAYER 1'S MARK</p>
+            </Container>
         </Container>
     )
 }
